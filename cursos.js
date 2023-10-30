3bb020f05a5c92b6df58df8ec4f55aab1fc75a3a
uesleireis07 <davimaciel077@gmail.com>
2023-10-30 10:58:28 -0300

    firebase

---
+++ cursos.js
@@ -0,0 +1,45 @@
+var dataRef = new Firebase('https://escola-a300d-default-rtdb.firebaseio.com');
+
+dataRef.on('value', function(snapshot) {
+
+    var t = snapshot.val();
+    var count = 0;
+
+    for (var key in t) {
+        var valor = t[key];
+
+        console.log('chave', key);
+        console.log('valor', valor);
+
+        if(key === 'cursos') {
+
+            var _itemsKey = Object.entries(valor);
+    
+    
+    
+            for (const [key2, value2] of _itemsKey) {
+                count++;
+    
+                $("#tabela tbody").append(
+    
+                    `<tr> +
+    
+                        <th scope="row">${count}</th> +
+    
+                        <td>${value2.nome}</td> +
+    
+                        <td>${value2.id_professores}</td> +
+    
+                        <td>${value2.ids_disciplina}</td> +
+    
+                    </tr>`
+    
+                );
+    
+            }
+        }
+
+
+    }
+
+});
---
+++ professores.js
@@ -0,0 +1,47 @@
+var dataRef = new Firebase('https://escola-a300d-default-rtdb.firebaseio.com');
+
+dataRef.on('value', function(snapshot) {
+
+    var t = snapshot.val();
+    var count = 0;
+
+    for (var key in t) {
+        var valor = t[key];
+
+        console.log('chave', key);
+        console.log('valor', valor);
+
+        if(key === 'professores') {
+
+            var _itemsKey = Object.entries(valor);
+    
+    
+    
+            for (const [key2, value2] of _itemsKey) {
+                count++;
+    
+                $("#tabela tbody").append(
+    
+                    `<tr> +
+    
+                        <th scope="row">${count}</th> +
+    
+                        <td>${value2.nome}</td> +
+    
+                        <td>${value2.telefone}</td> +
+    
+                        <td>${value2.formacao}</td> +
+
+                        <td>${value2.email}</td> +
+    
+                    </tr>`
+    
+                );
+    
+            }
+        }
+
+
+    }
+
+});
